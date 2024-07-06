948f2807a304f2ec0c4696763c51afb6877312d7
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2024-07-06 23:25:37 +0000

    initial comit

---
+++ Client/app/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+export const store = configureStore({
+    reducer: {
+   
+    },
+    
+  });
+
+  export type AppDispatch = typeof store.dispatch;
+  export type RootState = ReturnType<typeof store.getState>;
+  
+
+  setupListeners(store.dispatch);
---
+++ Client/src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+
+  return (
+
+      
+      <h1 className=" text-center underline font-bold">
+        Hello Spring boot and React 
+      </h1>
+  
+  )
+}
+
+export default App
